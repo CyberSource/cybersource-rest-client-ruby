65b86ec34f568bd05d13cdd2412863307e3d3a13
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2019-02-27 19:41:38 +0530

    + Fixing Improper Exception Handling

--- AuthenticationSDK/util/ApiException.rb
+++ AuthenticationSDK/util/ApiException.rb
@@ -1,19 +1,29 @@
 public
 class ApiException
   def apiexception(err,log_Obj)
-    log_Obj.logger.error(err.message)
-    if !err.backtrace.to_s.empty?
-      log_Obj.logger.error(err.backtrace)
-    end
-    log_Obj.logger.info('END> =======================================')
-    puts 'Check log for more details.'
+	begin
+		raise
+			log_Obj.logger.error(err.message)
+			if !err.backtrace.to_s.empty?
+			  log_Obj.logger.error(err.backtrace)
+			end
+			log_Obj.logger.info('END> =======================================')
+			puts 'Check log for more details.'
+		rescue
+			puts 'Cannot access error details.'
+	end
   end
   def apiwarning(message,log_Obj)
     log_Obj.logger.warn(message)
   end
   def customerror(message,log_Obj)
-    log_Obj.logger.error(message)
-    log_Obj.logger.info('END> =======================================')
-    puts 'Check log for more details.'
+	begin
+		raise
+			log_Obj.logger.error(message)
+			log_Obj.logger.info('END> =======================================')
+			puts 'Check log for more details.'
+		rescue
+			puts 'Cannot access error details.'
+	end
   end
 end
