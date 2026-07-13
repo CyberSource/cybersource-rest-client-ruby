d5840d79e9bdf492f4b6678bee292f65365ef824
ShepFC3 <chris.shepherd@flybreeze.com>
2026-07-13 11:23:36 -0600

    Adds return_code to MLE response

--- lib/cybersource_rest_client/api_client.rb
+++ lib/cybersource_rest_client/api_client.rb
@@ -83,7 +83,8 @@ module CyberSource
       if MLEUtility.check_is_mle_encrypted_response(response.body)
         begin
           body = MLEUtility.decrypt_mle_response_payload(@merchantconfig, response.body)
-          response = Typhoeus::Response.new(:code => response.code, :headers => response.headers.to_hash, :body => body)
+          response = Typhoeus::Response.new(:code => response.code, :return_code => response.return_code,
+                                            :headers => response.headers.to_hash, :body => body)
         rescue => e
           raise ApiError.new(:message => "MLE Encrypted Response Decryption Error Occurred. Error: #{e.message}",
                             :code => response.code,
